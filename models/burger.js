097634db84244deeed3cb2346f028e29e7bdf977
myrean <dhughes1@uci.edu>
2019-02-12 22:26:26 -0800

    added connection to server.js

---
+++ models/burger.js
@@ -0,0 +1,6 @@
+//Dependencies
+//=====================
+var orm = require("../config/orm");
+
+/*create the code that calls ORM functions using 
+burger specific input for the ORM. Export at end.*/
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 /*Overview
-In this assignment, you'll create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). Be sure to follow the MVC design pattern; use Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.*/
+In this assignment, you'll create a burger logger 
+with MySQL, Node, Express, Handlebars and a homemade 
+ORM (yum!). Be sure to follow the MVC design pattern; 
+use Node and MySQL to query and route data in your 
+app, and Handlebars to generate your HTML.*/
 
 var mysql = require("mysql");
+var express = require("express");
+var connection = require("./config/connection");
 
