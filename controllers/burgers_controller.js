ca9e71b599b2a492cc102e83e5e7e2a8363f168e
myrean <dhughes1@uci.edu>
2019-02-24 11:26:00 -0800

    refresh fixed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,24 +18,9 @@ router.get("/", function (req, res) {
     });
 });
 
-//I dont think you can do this --
-// router.get("/:burger_name", function (req, res) {
-//     burger.all(function (data) {
-//         var burgersData = {
-//             burgers: data
-//         };
-//         console.log(burgersData)
-//         for (var i = 0; i < burgersData.length; i++) {
-//             if (burgersData[i].burger_name == req.params.burger_name) {
-//                 return res.render("index", burgersData[i]);
-//             }
-//         }
-//     })
-// });
-
 //create a burger, push to mysql, redirect to / to show new burger
 router.post("/api/create", function (req, res) {
-    console.log(req.body.burgerText);
+    
     burger.create([
         "burger_name", "devoured"
     ], [
@@ -43,25 +28,20 @@ router.post("/api/create", function (req, res) {
         ], function (result) {
           res.redirect("/");
             
-            //   res.json({ id: result.insertId });
         });
 });
-//use this to do the buttons
-//   router.put("/api/burger/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
 
-//     console.log("condition", condition);
+//update the db to devoured true
+  router.post("/api/update/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+   console.log(req.params.id);
+    burger.update({
+      devoured: 1
+    }, condition, function(result) {
+        res.redirect("/");
+      console.log(result);
+      })
+    });
 
-//     burger.update({
-//       devoured: req.body.devoured
-//     }, condition, function(result) {
-//       if (result.changedRows == 0) {
-
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
-
+//export the routes / router
 module.exports = router;
--- public/assets/javascript/burgers.js
+++ public/assets/javascript/burgers.js
@@ -1,7 +1,27 @@
+$(".burgBtn").on("click", function(event) {
+    event.preventDefault();
+    
+    var id = $(this).attr("data-id")
+   console.log(id)
+ $.ajax("/api/update/" + id, {
+    type: "POST",
+    data: id
+  })
+  .then(
+    function() {
+        location.reload();
+      })
+  .catch(
+      function(err){
+          if (err) throw err;
+      }
+  )
+  
+    
+});
 
-
-$.get("/", function(data) {
-    console.log(data)
+// $.get("/", function(data) {
+//     console.log(data)
 
     //on clicks for button  to print "hello" first -- 
     //then get it to console log the ID to clientside console
@@ -9,4 +29,4 @@ $.get("/", function(data) {
     //post the backend to update devour to "1" where ID 
     // is the burgers devour
 
-})
+// })
